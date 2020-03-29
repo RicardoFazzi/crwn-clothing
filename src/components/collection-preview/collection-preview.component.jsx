2d2f6d2ad9c27661676a979cac03f045c1a54de1
Ricardo Fazzi <ricardofazzi007@gmail.com>
2020-03-29 12:48:22 -0300

    Added directory sections to store

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -8,7 +8,7 @@ const CollectionPreview = ({ title, items }) => {
       <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
         {items
-          .filter((item, index) => item.id <= 4)
+          .filter((item, index) => index < 4)
           .map((item) => (
             <CollectionItem key={item.id} item={item}></CollectionItem>
           ))}
