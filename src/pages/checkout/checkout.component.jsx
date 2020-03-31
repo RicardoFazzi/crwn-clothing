4124ac03dd438445122ab285984ddb912fb86bdb
Ricardo Fazzi <ricardofazzi007@gmail.com>
2020-03-31 12:08:39 -0300

    Integrated Stripe for payments

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -7,6 +7,7 @@ import {
   selectCartTotal
 } from "../../redux/cart/cart.selectors";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 const CheckoutPage = ({cartItems, total}) => (
   <div className="checkout-page">
@@ -33,6 +34,11 @@ const CheckoutPage = ({cartItems, total}) => (
     <div className='total'>
       <span>Total: ${total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following card for test payments <br/>
+      4242 4242 4242 4242 - Exp 07/20 - CVV 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
