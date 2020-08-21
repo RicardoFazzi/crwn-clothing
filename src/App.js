0d410f06144e6716ecb6e8930333807294479999
Ricardo Fazzi <ricardofazzi007@gmail.com>
2020-08-21 17:05:31 -0300

    Mobile support

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import { GlobalStyles } from "./global.styles";
 import HomePage from "./pages/homepage/homepage.component";
 import { Route, Switch, Redirect } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
@@ -20,6 +20,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <GlobalStyles/>
         <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
